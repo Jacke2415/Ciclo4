e293b7ea7c2f351a602440a14a06464c99c3fdd5
Pasovert <pasovert@gmail.com>
2021-11-24 18:48:31 -0500

    modificando proyecto..

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './components/Login/Login';
-import Particulas from './components/Login/particulas';
-import Panel from './components/Login/panel'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
-    <Panel />
-    <Particulas />
+    <Login />    
   </React.StrictMode>,
   document.getElementById('root')
 );
