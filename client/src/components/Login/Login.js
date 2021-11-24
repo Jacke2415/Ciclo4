49a43c39687f8dd1bc85f398046ee94fbf982968
Jackeline Narvaez <jacke2415@gmail.com>
2021-11-24 14:55:15 -0500

    pagina principal en react

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,19 @@
+
+import '../../public/css/Login.css'
+import FormLogin from './formLogin';
+
+export default function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        <div class="header">
+          <div>Nominas</div>
+        </div>
+          <FormLogin />
+        <div id="particles-js">
+          
+        </div>
+      </header>
+    </div>
+  );
+}
