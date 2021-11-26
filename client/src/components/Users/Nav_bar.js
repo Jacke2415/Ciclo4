f3ed135eb6e78a1e1f1db25f3d3134e06313015a
rusian20 <mezadavid1202@gmail.com>
2021-11-26 18:53:35 -0500

    estructura nueva

--- client/src/components/Users/Nav_bar.js
+++ client/src/components/Users/Nav_bar.js
@@ -1,39 +1,32 @@
 import React from 'react';
-/* import { Button } from 'react-bootstrap';*/
-/* import './'; */
+import '../../public/css/Nav_bar.css';
 
-class Nav_bar extends React.Component {
+
+class Navbar extends React.Component {
 
     render() {
 
         return (
-            <><div className="Accordion">
-                <ul className="show-dropdown main-navbar">
-                    <div className="selector-active">
-                        <div className="top"></div>
-                        <div className="bottom"></div>
+            <>
+                <div class="d-flex align-items-start">
+                    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z" />
+                        </svg> Home</button>
+                        <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                        <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                        <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
                     </div>
-                    <img className="mx-lg-5" src="./nominas.png" weight="90px" height="90px"
-                        style="margin-left: 20px; border-radius: 10px;"></img>
-                    <li className="active">
-                        <a href="javascript:void(0);"><i class ="fas fa-user-edit"></i>Perfil Usuario</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);"><i class ="fas fa-money-check-alt"></i>Reporte Nomina</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);"><i class ="far fa-calendar-alt"></i>Solicitar Vacaciones</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);"><i class ="far fa-file"></i>Solicitar Certificados</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0)"><i class ="far fa-clipboard"></i>Solicitar Permisos</a>
-                    </li>
-                </ul>
-            </div></>
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                    </div>
+                </div>
+            </>
 
         );
     }
 }
-export default Nav_bar;
+export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './components/Login/Login';
+/* import Login from './components/Login/Login'; */
+import Navbar from './components/Users/Nav_bar';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />    
+    {/* <Login />   */}  
+    <Navbar />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
