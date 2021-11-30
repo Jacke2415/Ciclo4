278a1295468194cdfd4a4d875e0861d5c1af0892
rusian20 <mezadavid1202@gmail.com>
2021-11-29 22:21:47 -0500

    perfil de usuario creado

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import CrearNomina from './components/User/UserNomina/CrearNomina';
-import { DataTableDemo } from './components/User/UserNomina/DataTableDemo';
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <CrearNomina />
+    
     
 
-    {/*<Login />*/ }   
+     
   </React.StrictMode>,
   document.getElementById('root')
 );
