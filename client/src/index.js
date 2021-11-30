d8265acb6e69f93dd64a6020c8f9ab1cf924d6b9
lmercado80 <lmercado80@gmail.com>
2021-11-30 00:32:47 -0500

    Eliminar dataTableDemo

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import CrearNomina from './components/User/UserNomina/CrearNomina';
 import SupMisNominas from './components/User/UserNomina/SupMisNominas'
 import {TablaFuncionario} from './components/User/UserNomina/TablaFuncionario'
+import {TablaNominas} from './components/User/UserNomina/TablaNominas'
 
 
 ReactDOM.render(
@@ -12,7 +13,7 @@ ReactDOM.render(
     //<CrearNomina />
     <SupMisNominas />
     <TablaFuncionario />
-
+    <TablaNominas />
    // {/*<Login />*/ }   
   </React.StrictMode>,
   document.getElementById('root')
