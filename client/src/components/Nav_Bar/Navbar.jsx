831afa255a07ee1466eceb88433153d01fe9cbd4
rusian20 <mezadavid1202@gmail.com>
2021-12-01 22:29:56 -0500

    actualizacion del nav_bar, mejor diseño

--- client/src/components/Nav_Bar/Navbar.jsx
+++ client/src/components/Nav_Bar/Navbar.jsx
@@ -9,32 +9,98 @@ class Navbar extends React.Component {
 
         return (
             <>
-                <div className="wrapper">
-                    <nav id="sidebar">
-                        <div className="sidebar-header">
-                            <img src={nominas} weight="90px" height="90px" style={{marginLeft:"20px", borderRadius:"10px",}}/>
-                        </div>
+                <div class="area"></div><nav class="main-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-home fa-2x"></i>
+                                <span class="nav-text">
+                                    Dashboard
+                                </span>
+                            </a>
 
-                        <ul className="list-unstyled components">
-                            <p>Plataforma Nomina</p>
-                            
-                            <li>
-                                <a href="#">About</a>
-                            </li>
-                            
-                            <li>
-                                <a href="#">Portfolio</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
-                            <li>
-                                
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                
+                        </li>
+                        <li className="has-subnav">
+                            <a href="#">
+                                <i className="fa fa-laptop fa-2x"></i>
+                                <span className="nav-text">
+                                    Stars Components
+                                </span>
+                            </a>
+
+                        </li>
+                        <li className="has-subnav">
+                            <a href="#">
+                                <i className="fa fa-list fa-2x"></i>
+                                <span className="nav-text">
+                                    Forms
+                                </span>
+                            </a>
+
+                        </li>
+                        <li className="has-subnav">
+                            <a href="#">
+                                <i className="fa fa-folder-open fa-2x"></i>
+                                <span className="nav-text">
+                                    Pages
+                                </span>
+                            </a>
+
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-bar-chart-o fa-2x"></i>
+                                <span className="nav-text">
+                                    Graphs and Statistics
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-font fa-2x"></i>
+                                <span className="nav-text">
+                                    Quotes
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-table fa-2x"></i>
+                                <span className="nav-text">
+                                    Tables
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-map-marker fa-2x"></i>
+                                <span className="nav-text">
+                                    Maps
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-info fa-2x"></i>
+                                <span className="nav-text">
+                                    Documentation
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul className="logout">
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-power-off fa-2x"></i>
+                                <span className="nav-text">
+                                    Logout
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
             </>
 
         );
