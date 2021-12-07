d84b5e228b122f2abe7d6caad82d958eed5f89d5
rusian20 <mezadavid1202@gmail.com>
2021-12-06 21:53:30 -0500

    reporte de pago falta una vista

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 /* import CrearNomina from './components/User/UserNomina/CrearNomina'; */
 /* import { DataTableDemo } from './components/User/UserNomina/DataTableDemo'; */
- import Navbar from './components/Nav_Bar/Navbar.jsx';
+import Navbar from './components/Nav_Bar/Navbar.jsx';
 import Login from './pages/Login'; 
 import PerfilUser from './components/User/UserEmpleado/PerfilUser.jsx'
-import SoliVac from './components/User/UserEmpleado/SolicitarVac'
+import SoliRep from './components/User/UserEmpleado/SolicitaRep'
 
 ReactDOM.render(
   <React.StrictMode>
     
     <BrowserRouter>
       <Routes>
-        <Route path = '/' element = {< SoliVac/>} />
+        <Route path = '/' element = {< SoliRep/>} />
         <Route path = '/singin' element = {<Login />} />
         <Route path = '/singin/user' element = {<Navbar />} />    
       </Routes>
