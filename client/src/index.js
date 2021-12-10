0c9a4e632aa1f828e0e1a3e6f9fb86f61257e8dd
rusian20 <mezadavid1202@gmail.com>
2021-12-09 22:30:46 -0500

    pull con la rama de luis

--- client/src/index.js
+++ client/src/index.js
@@ -6,20 +6,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 /* import CrearNomina from './components/User/UserNomina/CrearNomina'; */
 /* import { DataTableDemo } from './components/User/UserNomina/DataTableDemo'; */
-import Navbar from './components/Nav_Bar/Navbar.jsx';
 import Login from './pages/Login'; 
-import PerfilUser from './components/User/UserEmpleado/PerfilUser.jsx'
+/* import PerfilUser from './components/User/UserEmpleado/PerfilUser.jsx' */
 import SoliRep from './components/User/UserEmpleado/SolicitaRep'
 import SolicitarPer from './components/User/UserEmpleado/SolicitarPermiso'
+import SoliVac from './components/User/UserEmpleado/SolicitarVac'
 
 ReactDOM.render(
   <React.StrictMode>
     
     <BrowserRouter>
       <Routes>
-        <Route path = '/' element = {< PerfilUser/>} />
-        <Route path = '/singin' element = {<Login />} />
-        <Route path = '/singin/user' element = {<Navbar />} />    
+        <Route path = '/' element = {< Login/>} />
+        <Route path = '/singin/user' element = {<SoliVac/>} />
         <Route path = '/solirep' element = {<SoliRep/>} />
         <Route path = '/soliper/' element = {<SolicitarPer/>}/>
       </Routes>
