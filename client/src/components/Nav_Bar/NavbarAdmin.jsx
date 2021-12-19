379951e3623d371f74457083c72ae86d106b96fa
Daniel Niebles <pasovert@gmail.com>
2021-12-19 08:36:45 -0500

    Arreglo del navbar para los perfiles (empleado, nomina y administrador)

--- client/src/components/Nav_Bar/NavbarAdmin.jsx
+++ client/src/components/Nav_Bar/NavbarAdmin.jsx
@@ -1,114 +1,48 @@
-import React from 'react';
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
-import '../../public/css/navbar.css';
-
-
+import React from "react";
+import DNavbar from "./Navbar.jsx";
+import {
+  svgperfil,
+  svgCreateNomina,
+  svgGestionarVacaciones,
+  svgCreateEmpleado,
+  svgGestionarPermisos,
+  svgSalario,
+} from "./LogosNavbar.jsx";
 
 const NavbarAdmin = () => {
-    const { currentUser } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
-
-    const logout = () => {
-    logout(dispatch);
-    };
-
-    console.log(currentUser)
-
-        return (
-            <>
-                <nav class="main-menu">
-                    <ul>
-                        <li>
-                            <Link to='/signin/administrador/perfilAdministrador'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                </svg>
-                                <span class="nav-text"> Perfil del Administrador
-                                </span>
-                            </Link>
-
-                        </li>
-                        
-                        <li className="has-subnav">
-                            <Link to='/signin/administrador/gestionarEmpleado'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-rolodex" viewBox="0 0 16 16">
-                                    <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                                    <path d="M1 1a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h.5a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5.5.5 0 0 1 1 0 .5.5 0 0 0 .5.5h.5a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H6.707L6 1.293A1 1 0 0 0 5.293 1H1Zm0 1h4.293L6 2.707A1 1 0 0 0 6.707 3H15v10h-.085a1.5 1.5 0 0 0-2.4-.63C11.885 11.223 10.554 10 8 10c-2.555 0-3.886 1.224-4.514 2.37a1.5 1.5 0 0 0-2.4.63H1V2Z"/>
-                                </svg>
-                                <span className="nav-text">
-                                   Gestionar Empleado
-                                </span>
-                            </Link>
-
-                        </li>
-                        <li className="has-subnav">
-                            <Link to='/signin/administrador/gestionarNomina'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
-                                    <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-                                </svg>
-                                <span className="nav-text">
-                                    Gestionar Nomina
-                                </span>
-                            </Link>
-
-                        </li>
-                        <li>
-                            <Link to='/signin/administrador/gestionarVacaciones'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                                </svg>  
-                                <span className="nav-text">
-                                    Gestionar Vacaciones
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/signin/administrador/gestionarPermisos'>
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
-                                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-                                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-                                </svg>
-                                <span className="nav-text">
-                                    Gestionar Permisos
-                                </span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/signin/administrador/generarReportes'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3-range" viewBox="0 0 16 16">
-                                <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
-                                <path d="M7 10a1 1 0 0 0 0-2H1v2h6zm2-3h6V5H9a1 1 0 0 0 0 2z"/>
-                            </svg>
-                                <span className="nav-text">
-                                    Generar Reportes
-                                </span>
-                            </Link>
-                        </li>
-                        
-                    </ul>
-
-                    <ul className="logout">
-                        <li>
-                            <Link to="/signin/logout" onClick={logout}>
-                                <i className="fa fa-power-off fa-2x"></i>
-                                <span className="nav-text">
-                                    Logout
-                                </span>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-
-            </>
-
-        );
-    }
-
+  const items = [
+    {
+      name: "Perfil de Usuario",
+      url: "/signin/administrador/perfilAdministrador",
+      svg: svgperfil(),
+    },
+    {
+      name: "Gestionar Empleado",
+      url: "/signin/administrador/gestionarEmpleado",
+      svg: svgCreateEmpleado(),
+    },
+    {
+      name: "Gestionar Nomina",
+      url: "/signin/administrador/gestionarNomina",
+      svg: svgCreateNomina(),
+    },
+    {
+      name: "Gestionar Vacaciones",
+      url: "/signin/administrador/gestionarVacaciones",
+      svg: svgGestionarVacaciones(),
+    },
+    {
+      name: "Gestionar Permisos",
+      url: "/signin/administrador/gestionarPermisos",
+      svg: svgGestionarPermisos(),
+    },
+    {
+      name: "Generar Reporte",
+      url: "/signin/administrador/generarReportes",
+      svg: svgSalario(),
+    },
+  ];
+  return <DNavbar items={items} />;
+};
 
 export default NavbarAdmin;
