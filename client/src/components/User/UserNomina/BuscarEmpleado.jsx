baefe53a4af8976771380a645de64cc3e7b8b0c6
kellyrmedrano <kellyrolonmedrano@gmail.com>
2021-12-21 00:32:06 -0500

    gestion vacaciones ok

--- client/src/components/User/UserNomina/BuscarEmpleado.jsx
+++ client/src/components/User/UserNomina/BuscarEmpleado.jsx
@@ -1,6 +1,5 @@
 import React  from "react"
 import axios from "axios";
-import { useEffect } from "react";
 import { useState } from "react";
 
 
@@ -31,48 +30,29 @@ export default function BuscarEmpleado() {
           
           <div className="col-3">
             <div className="form">
-              <input
-                type="text"
-                className="form-control"
-                id="cedula"
-                placeholder="No Identificación"
-                value={cedula}
-                onChange={(event) => {
-                  setCedula(event.target.value);
-                }}
-              />
-              
+              <input type="text" className="form-control"
+                id="cedula" placeholder="No Identificación"
+                value={cedula} onChange={(event) => {
+                  setCedula(event.target.value);}}/>
             </div>
           </div>
           <div className="col">
             <select
-              className="form-select mb-2"
-              aria-label="Default select example"
-              id="estado"
-              value={estado}
-              onChange={(event) => {
-                setEstado(event.target.value );
-              }}
-            >
+              className="form-select mb-2" aria-label="Default select example"
+              id="estado" value={estado} onChange={(event) => {
+                setEstado(event.target.value );}}>
               <option selected>Escoger</option>
               <option value="Aceptada">Aceptar</option>
               <option value="Rechazada">Rechazar</option>
             </select>
           </div>
-          
-          
-          
         </div>
-
         <div className="row">
           <div className="col-4">
             <div className="d-grid gap-2">
               <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={actualizarVacaciones}
-              >
-                Confirmar
+                type="submit" className="btn btn-primary"
+                onClick={actualizarVacaciones}> Confirmar
               </button>
             </div>
           </div>
